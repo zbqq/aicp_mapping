521c9a4b6bd1282ce6d5130d62c232f88690c9a8
simalpha <nobili.simona@gmail.com>
2017-03-20 17:39:39 +0000

    Command line program to test aicp.

--- src/registration/clouds_io_utils.h
+++ src/registration/clouds_io_utils.h
@@ -2,5 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <pcl/common/io.h>
+#include <pcl/io/pcd_io.h>
 
 int savePlanarCloudCSV (const std::string &file_name, const pcl::PCLPointCloud2 &cloud);
+
+void savePointCloudPCLwithPose(const std::string file_name, pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, Eigen::Isometry3d sensor_pose = Eigen::Isometry3d::Identity());
