86acf923826ef1f53c6a7cb2b844b40aaa6047e7
simalpha <simona.nobili@ed.ac.uk>
2017-08-26 17:57:04 +0100

    Preliminary version of SVM. Still to be tested.

---
+++ src/classification/include/abstract_classification.hpp
@@ -0,0 +1,19 @@
+#ifndef AICP_CLASSIFICATION_ABSTRACT_HPP_
+#define AICP_CLASSIFICATION_ABSTRACT_HPP_
+
+#include "common.hpp"
+#include <opencv2/core/core.hpp>
+
+namespace aicp {
+
+	class AbstractClassification {
+	public:
+	  virtual void train(const Eigen::MatrixXd &training_data, const Eigen::MatrixXd &labels) = 0;
+	  virtual void test(const Eigen::MatrixXd &testing_data, const Eigen::MatrixXd &labels, Eigen::MatrixXd *probabilities = NULL) = 0;
+	  virtual void save(const std::string &filename) = 0;
+	  virtual void load(const std::string &filename) = 0;
+	};
+
+}
+
+#endif
