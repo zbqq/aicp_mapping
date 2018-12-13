0a7b0e46d13aceff6c93424e9dc0c30950ce32c8
simalpha <simona.nobili@ed.ac.uk>
2018-12-13 18:36:38 +0000

    AICP rgistration: fully working.

--- src/registration_apps/include/registration_apps/app.hpp
+++ src/registration_apps/include/registration_apps/app.hpp
@@ -32,8 +32,8 @@ struct CommandLineConfig
     string output_channel;
     string working_mode;
     bool failure_prediction_mode;
+    int reference_update_frequency;
     bool verbose;
-    bool apply_correction;
 };
 
 namespace aicp {
@@ -43,16 +43,13 @@ public:
     App(const CommandLineConfig& cl_cfg,
         RegistrationParams reg_params,
         OverlapParams overlap_params,
-        ClassificationParams class_params,
-        string exp_params);
-
+        ClassificationParams class_params);
 
     ~App(){
     }
     RegistrationParams reg_params_;
     OverlapParams overlap_params_;
     ClassificationParams class_params_;
-    string exp_params_;
     int online_results_line_;
 
     // thread function doing actual work
@@ -62,7 +59,6 @@ protected:
     void paramInit(){
         pose_initialized_ = FALSE;
         updated_correction_ = TRUE;
-        rejected_correction = TRUE;
 
         valid_correction_ = FALSE;
         force_reference_update_ = FALSE;
@@ -84,9 +80,8 @@ protected:
 
         local_ = Eigen::Isometry3d::Identity();
         world_to_body_msg_ = Eigen::Isometry3d::Identity();
-//        world_to_body_corr_first_ = Eigen::Isometry3d::Identity();
         corrected_pose_ = Eigen::Isometry3d::Identity(); // world_to_body corrected (world -> body)
-        current_correction_ = Eigen::Isometry3d::Identity();
+        total_correction_ = Eigen::Isometry3d::Identity();
     }
 
     const CommandLineConfig cl_cfg_;
@@ -100,7 +95,6 @@ protected:
     std::thread worker_thread_;
     std::condition_variable worker_condition_;
     std::mutex worker_mutex_;
-//    std::list<pcl::PointCloud<pcl::PointXYZ>::Ptr> data_queue_;
     std::list<AlignedCloudPtr> cloud_queue_;
     std::mutex data_mutex_;
     std::mutex robot_state_mutex_;
@@ -116,18 +110,11 @@ protected:
 
     // Transformation matrices
     Eigen::Isometry3d local_;
-//    Eigen::Isometry3d body_to_lidar_; // Variable tf from the lidar to the robot's base link
-//    Eigen::Isometry3d head_to_lidar_; // Fixed tf from the lidar to the robot's head frame
     Eigen::Isometry3d world_to_body_msg_; // Captures the position of the body frame in world from launch
                                           // without drift correction
-//    long long int world_to_body_msg_utime_;
-//    long long int world_to_body_msg_utime_first_;
-//    Eigen::Isometry3d world_to_head_; // running head position estimate
-//    Eigen::Isometry3d world_to_body_corr_first_;
 
     bool pose_initialized_;
     bool updated_correction_;
-    bool rejected_correction;
 
     // Clear buffer after pose update (to avoid distortion)
     bool clear_clouds_buffer_;
@@ -148,7 +135,7 @@ protected:
     bool valid_correction_;
     bool force_reference_update_;
     Eigen::Isometry3d corrected_pose_;
-    Eigen::Isometry3d current_correction_;
+    Eigen::Isometry3d total_correction_;
 
     // Reference cloud update counters
     int updates_counter_;
