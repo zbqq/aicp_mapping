3a839950d3bb9ff191ce72b1f3965abdb2a796c2
Maurice Fallon <mauricefallon@gmail.com>
2019-04-24 21:41:31 +0100

    drop messages if we get behind

--- aicp_ros/src/app_ros.cpp
+++ aicp_ros/src/app_ros.cpp
@@ -180,7 +180,7 @@ void AppROS::velodyneCallBack(const sensor_msgs::PointCloud2::ConstPtr &laser_ms
 //            vis_->publishCloud(accumulated_cloud, 10, "/aicp/accumulated_cloud", accu_->getFinishedTime());
 
             // Push this cloud onto the work queue (mutex safe)
-            const int max_queue_size = 100;
+            const int max_queue_size = 3;
             {
                 std::unique_lock<std::mutex> lock(data_mutex_);
 
@@ -194,6 +194,8 @@ void AppROS::velodyneCallBack(const sensor_msgs::PointCloud2::ConstPtr &laser_ms
                 cloud_queue_.push_back(current_cloud);
 
                 if (cloud_queue_.size() > max_queue_size) {
+                    cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n";
+                    cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n";
                     cout << "[App ROS] WARNING: dropping " <<
                             (cloud_queue_.size()-max_queue_size) << " clouds." << endl;
                 }
